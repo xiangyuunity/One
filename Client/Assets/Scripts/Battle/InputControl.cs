5aac4251042e572aae3f2f6790b9efd7eed97d31
xiangyuunity <982953560@qq.com>
2018-02-22 19:33:38 +0800

    init

---
+++ Client/Assets/Scripts/Battle/InputControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControl : MonoBehaviour
+{
+    public float MoveSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ControlMove();
+    }
+
+    void ControlMove()
+    {
+#if UNITY_EDITOR
+        KeyControl();
+#else
+		GravityControl();
+#endif
+    }
+
+    private void KeyControl()
+    {
+        float deltaX = Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime;
+        float deltaZ = Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        transform.position = transform.position + movement;
+    }
+    private void GravityControl()
+    {
+        float deltaX = Input.acceleration.x * MoveSpeed * Time.deltaTime;
+        float deltaZ = Input.acceleration.y * MoveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        transform.position = transform.position + movement;
+    }
+
+}
